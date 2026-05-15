b894106b24f339cea28a55405f63910eeca3894b
RobaMamdouh <roba.mmahfouz@gmail.com>
2026-05-15 16:27:51 +0300

    Added Components, Services and Models

---
+++ memoryScramble/src/app/components/hud/hud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hud',
+  standalone: true,
+  imports: [],
+  templateUrl: './hud.component.html',
+  styleUrl: './hud.component.scss'
+})
+export class HudComponent {
+
+}
