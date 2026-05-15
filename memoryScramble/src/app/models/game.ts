93878c2138b9e429d873c6d407c163bfc1b8d125
RobaMamdouh <roba.mmahfouz@gmail.com>
2026-05-15 17:19:42 +0300

    Added interfaces and Card emojis
    Added the interfaces that will be used in the services as well as the pictures that will appear on the cards when flipped

--- memoryScramble/src/app/models/game.ts
+++ memoryScramble/src/app/models/game.ts
@@ -1,2 +1,31 @@
-export interface Game {
+export interface BoardConfig {
+  nRows: number;
+  nColumns: number;
+  timeoutSeconds: number;
+}
+
+export interface Card {
+  id: number;
+  icon: string;
+  pairId: number;
+  isFlipped: boolean;
+  isMatched: boolean;
+}
+
+export enum GameStatus {
+  IDLE = 'idle',
+  PLAYING = 'playing',
+  WON = 'won',
+  LOST = 'lost',
+}
+
+export interface GameState {
+  config: BoardConfig;
+  cards: Card[];
+  status: GameStatus;
+  timeLeft: number;
+  moves: number;
+  matchedPairs: number;
+  totalPairs: number;
+  flippedCardIds: number[];
 }
